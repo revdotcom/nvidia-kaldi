ce41be56f044b8b3a9bb9aabe84f7a528a49ec7e
Justin Luitjens <jluitjens@nvidia.com>
2019-01-02 08:58:05 -0800

    Resolve "Restructure changes"

---
+++ src/cudadecoder/cuda-decodable.h
@@ -0,0 +1,183 @@
+// cudadecoder/cuda-decodable.h
+// TODO nvidia apache2
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+// THIS CODE IS PROVIDED *AS IS* BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION ANY IMPLIED
+// WARRANTIES OR CONDITIONS OF TITLE, FITNESS FOR A PARTICULAR PURPOSE,
+// MERCHANTABLITY OR NON-INFRINGEMENT.
+// See the Apache 2 License for the specific language governing permissions and
+// limitations under the License.
+
+
+#ifndef KALDI_CUDA_DECODEABLE_H_
+#define KALDI_CUDA_DECODEABLE_H_
+
+#include "feat/wave-reader.h"
+#include "online2/online-nnet2-feature-pipeline.h"
+#include "cudadecoder/cuda-decoder.h"
+#include "nnet3/decodable-simple-looped.h"
+#include "nnet3/decodable-online-looped.h"
+#include <atomic>
+#include <thread>
+#include <nvToolsExt.h>
+
+namespace kaldi {
+
+	/* BatchedCudaDecoderConfig
+	 * This class is a common configuration class for the various components
+	 * of a batched cuda multi-threaded pipeline.  It defines a single place
+	 * to control all operations and ensures that the various componets
+	 * match configurations
+	 */
+	//configuration options common to the BatchedCudaDecoder and BatchedCudaDecodable
+	struct BatchedCudaDecoderConfig {
+		BatchedCudaDecoderConfig() : max_batch_size_(20) {};
+		void Register(ParseOptions *po) {
+			feature_opts_.Register(po);
+			decodable_opts_.Register(po);
+			decoder_opts_.Register(po);
+			po->Register("max-batch-size",&max_batch_size_, "The maximum batch size to be used by the decoder.");
+			po->Register("num-threads",&num_threads_, "The number of workpool threads to use in the ThreadedBatchedCudaDecoder");
+			decoder_opts_.nlanes=max_batch_size_;
+			decoder_opts_.nchannels=max_batch_size_;
+
+		}
+		int max_batch_size_;
+		int num_threads_;
+
+		OnlineNnet2FeaturePipelineConfig  feature_opts_;           //constant readonly
+		nnet3::NnetSimpleLoopedComputationOptions decodable_opts_; //constant readonly
+		CudaDecoderConfig decoder_opts_;                           //constant readonly
+	};
+
+	class DecodableAmNnetLoopedOnlineCuda: public nnet3::DecodableNnetLoopedOnlineBase, public CudaDecodableInterface  {
+		public:
+			DecodableAmNnetLoopedOnlineCuda(
+					const nnet3::DecodableNnetSimpleLoopedInfo &info,
+					OnlineFeatureInterface *input_features,
+					OnlineFeatureInterface *ivector_features) :  DecodableNnetLoopedOnlineBase(info, input_features, ivector_features)  {};
+
+			~DecodableAmNnetLoopedOnlineCuda() {};
+
+			//returns cuda pointer to nnet3 output
+			virtual BaseFloat* GetLogLikelihoodsCudaPointer(int32 subsampled_frame) {
+				EnsureFrameIsComputed(subsampled_frame);
+				cudaStreamSynchronize(cudaStreamPerThread);      
+
+				BaseFloat *frame_nnet3_out = current_log_post_.Data()+(subsampled_frame-current_log_post_subsampled_offset_)*current_log_post_.Stride();
+				return frame_nnet3_out;
+			};
+
+			//DecodableInterface that should never be called.  Is inherited from DecodableNnetLoopedOnlineBase
+			virtual int32 NumIndices() const { KALDI_ASSERT(false); return 0; } 
+			virtual BaseFloat LogLikelihood(int32 subsampled_frame, int32 transition_id) { KALDI_ASSERT(false); return 0; } 
+
+			virtual bool IsLastFrame(int32 subsampled_frame) const { return  nnet3::DecodableNnetLoopedOnlineBase::IsLastFrame(subsampled_frame); }
+			virtual int32 NumFramesReady() const { return nnet3::DecodableNnetLoopedOnlineBase::NumFramesReady(); }
+
+		private:
+			KALDI_DISALLOW_COPY_AND_ASSIGN(DecodableAmNnetLoopedOnlineCuda);
+	};
+
+	/*
+	 *  ThreadedBatchedCudaDecoder uses multiple levels of parallelism in order to decode quickly on CUDA GPUs.
+	 *  It's API is utterance centric using deferred execution.  That is a user submits work one utterance at a time
+	 *  and the class batches that work behind the scene. Utterance are passed into the API with a unique key of type string.
+	 *  The user must ensure this name is unique.  APIs are provided to enqueue work, query the best path, and cleanup enqueued work.
+	 *  Once a user closes a decode handle they are free to use that key again.
+	 *  
+	 *  Example Usage is as follows:
+	 *  ThreadedBatchedCudaDecoder decoder;
+	 *  decoder.Initalize(decode_fst, am_nnet_rx_file);
+	 *   
+	 *  //some loop
+	 *    std::string utt_key = ...
+	 *    while(!decoder.OpenDecodeHandle(utt_key,wave_data));
+	 *
+	 *
+	 *  //some loop
+	 *    Lattice lat;
+	 *    std::string utt_key = ...
+	 *    decoder.GetBestPath(utt_key,&lat);
+	 *    decoder.CloseDecodeHandle(utt_key);
+	 *
+	 *  decoder.Finalize();
+	 */
+	class ThreadedBatchedCudaDecoder {
+    public:
+
+      ThreadedBatchedCudaDecoder(const BatchedCudaDecoderConfig &config) : config_(config), max_pending_tasks_(2000) {};
+
+      void Register(ParseOptions &po) {
+        po.Register("max-outstanding-queue-length", &max_pending_tasks_, 
+            "Number of files to allow to be outstanding at a time.  When the number of files is larger than this handles will be closed before opening new ones in FIFO order.");
+      }
+
+      //TODO should this take an nnet instead of a string?
+      //allocates reusable objects that are common across all decodings
+      void Initialize(const fst::Fst<fst::StdArc> &decode_fst, std::string nnet3_rxfilename);
+      //deallocates reusable objects
+      void Finalize();
+
+      //query a specific key to see if compute on it is complete
+      bool isFinished(const std::string &key);
+
+      //remove an audio file from the decoding and clean up resources
+      void CloseDecodeHandle(const std::string &key);
+
+      //Adds a decoding task to the decoder
+      bool OpenDecodeHandle(const std::string &key, const WaveData &wave_data);
+
+      //Copies the best path for decoded handle "key" into lat
+      void GetBestPath(const std::string &key, Lattice *lat);
+
+      inline int NumPendingTasks() {
+        return (tasks_back_ - tasks_front_ + max_pending_tasks_+1) % (max_pending_tasks_+1); 
+      };
+
+    private:
+
+      //State needed for each decode task.  
+      struct TaskState {
+        WaveData wave_data;   //Wave data input
+        Lattice lat;          //Lattice output
+        std::atomic<bool> finished;  //Tells master thread if task has finished execution
+
+        TaskState() : finished(false) {};
+        void Init(const WaveData &wave_data_in) { wave_data=wave_data_in; finished=false; };
+      };
+
+      //Thread execution function.  This is a single worker thread which processes input.
+      void ExecuteWorker(int threadId);
+
+      const BatchedCudaDecoderConfig &config_;
+
+      int max_pending_tasks_; 
+
+      CudaFst cuda_fst_;
+      TransitionModel trans_model_;
+      nnet3::AmNnetSimple am_nnet_;
+      nnet3::DecodableNnetSimpleLoopedInfo *decodable_info_;
+
+      std::mutex tasks_mutex_;                      //protects tasks_front_ and pending_task_queue_ for workers
+      std::atomic<int> tasks_front_, tasks_back_;
+      TaskState** pending_task_queue_;
+
+      std::atomic<bool> exit_;                      //signals threads to exit
+      std::atomic<int> numStarted_;                 //signals master how many threads have started
+
+      std::map<std::string,TaskState> tasks_lookup_; //Contains a map of utterance to TaskState
+      std::vector<std::thread> thread_contexts_;     //A list of thread contexts
+	};
+
+
+
+} // end namespace kaldi.
+
+
+#endif
---
+++ src/cudadecoderbin/batched-wav-nnet3-cuda.cc
@@ -0,0 +1,247 @@
+// online2bin/online2-wav-nnet3-latgen-faster.cc
+
+// Copyright 2014  Johns Hopkins University (author: Daniel Povey)
+//           2016  Api.ai (Author: Ilya Platonov)
+
+// See ../../COPYING for clarification regarding multiple authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+// THIS CODE IS PROVIDED *AS IS* BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION ANY IMPLIED
+// WARRANTIES OR CONDITIONS OF TITLE, FITNESS FOR A PARTICULAR PURPOSE,
+// MERCHANTABLITY OR NON-INFRINGEMENT.
+// See the Apache 2 License for the specific language governing permissions and
+// limitations under the License.
+
+#include "cudamatrix/cu-allocator.h"
+#include "cudadecoder/cuda-decodable.h"
+#include "fstext/fstext-lib.h"
+#include "lat/lattice-functions.h"
+#include "util/kaldi-thread.h"
+#include "nnet3/nnet-utils.h"
+#include <cuda_profiler_api.h>
+#include <cuda.h>
+#include <chrono>
+
+using namespace kaldi;
+
+void GetDiagnosticsAndPrintOutput(const std::string &utt,
+                                  const fst::SymbolTable *word_syms,
+                                  const CompactLattice &clat,
+                                  int64 *tot_num_frames,
+                                  double *tot_like) {
+  if (clat.NumStates() == 0) {
+    KALDI_WARN << "Empty lattice.";
+    return;
+  }
+  CompactLattice best_path_clat;
+  CompactLatticeShortestPath(clat, &best_path_clat);
+
+  Lattice best_path_lat;
+  ConvertLattice(best_path_clat, &best_path_lat);
+
+  double likelihood;
+  LatticeWeight weight;
+  int32 num_frames;
+  std::vector<int32> alignment;
+  std::vector<int32> words;
+  GetLinearSymbolSequence(best_path_lat, &alignment, &words, &weight);
+  num_frames = alignment.size();
+  likelihood = -(weight.Value1() + weight.Value2());
+  *tot_num_frames += num_frames;
+  *tot_like += likelihood;
+  KALDI_VLOG(2) << "Likelihood per frame for utterance " << utt << " is "
+                << (likelihood / num_frames) << " over " << num_frames
+                << " frames.";
+
+  if (word_syms != NULL) {
+    std::cerr << utt << ' ';
+    for (size_t i = 0; i < words.size(); i++) {
+      std::string s = word_syms->Find(words[i]);
+      if (s == "")
+        KALDI_ERR << "Word-id " << words[i] << " not in symbol table.";
+      std::cerr << s << ' ';
+    }
+    std::cerr << std::endl;
+  }
+}
+
+int main(int argc, char *argv[]) {
+  try {
+    using namespace kaldi;
+    using namespace fst;
+
+    typedef kaldi::int32 int32;
+    typedef kaldi::int64 int64;
+
+    const char *usage =
+      "Reads in wav file(s) and simulates online decoding with neural nets\n"
+      "(nnet3 setup), with optional iVector-based speaker adaptation and\n"
+      "optional endpointing.  Note: some configuration values and inputs are\n"
+      "set via config files whose filenames are passed as options\n"
+      "\n"
+      "Usage: online2-wav-nnet3-latgen-faster [options] <nnet3-in> <fst-in> "
+      "<wav-rspecifier> <lattice-wspecifier>\n";
+
+    std::string word_syms_rxfilename;
+
+    bool write_lattice = true;
+    int num_todo = -1;
+    int iterations=1;
+    ParseOptions po(usage);
+    int pipeline_length=2000; //length of pipeline of outstanding requests, this is independent of queue lengths in decoder
+
+    po.Register("write-lattice",&write_lattice, "Output lattice to a file.  Setting to false is useful when benchmarking.");
+    po.Register("word-symbol-table", &word_syms_rxfilename, "Symbol table for words [for debug output]");
+    po.Register("file-limit", &num_todo, 
+        "Limits the number of files that are processed by this driver.  After N files are processed the remaing files are ignored.  Useful for profiling.");
+    po.Register("iterations", &iterations, "Number of times to decode the corpus.  Output will be written only once.");
+    
+    //Multi-threaded CPU and batched GPU decoder
+    BatchedCudaDecoderConfig batchedDecoderConfig;
+
+    RegisterCuAllocatorOptions(&po);
+    batchedDecoderConfig.Register(&po);
+
+    po.Read(argc, argv);
+
+    if (po.NumArgs() != 4) {
+      po.PrintUsage();
+      return 1;
+    }
+    
+    g_cuda_allocator.SetOptions(g_allocator_options);
+    CuDevice::Instantiate().SelectGpuId("yes");
+    CuDevice::Instantiate().AllowMultithreading();
+    
+    ThreadedBatchedCudaDecoder CudaDecoder(batchedDecoderConfig);
+
+    std::string nnet3_rxfilename = po.GetArg(1),
+      fst_rxfilename = po.GetArg(2),
+      wav_rspecifier = po.GetArg(3),
+      clat_wspecifier = po.GetArg(4);
+
+    CompactLatticeWriter clat_writer(clat_wspecifier);
+    
+    fst::Fst<fst::StdArc> *decode_fst= fst::ReadFstKaldiGeneric(fst_rxfilename);
+
+    CudaDecoder.Initialize(*decode_fst, nnet3_rxfilename);
+
+    delete decode_fst;
+
+    fst::SymbolTable *word_syms = NULL;
+    if (word_syms_rxfilename != "")
+      if (!(word_syms = fst::SymbolTable::ReadText(word_syms_rxfilename)))
+        KALDI_ERR << "Could not read symbol table from file "
+          << word_syms_rxfilename;
+
+    int32 num_done = 0, num_err = 0;
+    double tot_like = 0.0;
+    int64 num_frames = 0;
+    double total_audio=0;
+    
+    nvtxRangePush("Global Timer");
+    auto start = std::chrono::high_resolution_clock::now(); //starting timer here so we can measure throughput without allocation overheads
+
+    std::queue<std::string> processed;
+    for (int iter=0;iter<iterations;iter++) {
+      SequentialTableReader<WaveHolder> wav_reader(wav_rspecifier);
+
+        for (; !wav_reader.Done(); wav_reader.Next()) {
+          nvtxRangePushA("Utterance Iteration");
+
+          std::string utt = wav_reader.Key();
+          printf("Utterance: %s\n", utt.c_str());
+
+          const WaveData &wave_data = wav_reader.Value();
+          total_audio+=wave_data.Duration();
+
+          while(!CudaDecoder.OpenDecodeHandle(utt,wave_data));
+          processed.push(utt);
+          num_done++;
+
+         while (processed.size()>=pipeline_length) {
+            std::string &utt = processed.front();
+            Lattice lat;
+            CompactLattice clat;
+        
+            CudaDecoder.GetBestPath(utt,&lat);
+            ConvertLattice(lat, &clat);
+
+            GetDiagnosticsAndPrintOutput(utt, word_syms, clat, &num_frames, &tot_like);
+
+            if (write_lattice && iter==0 ) {
+              clat_writer.Write(utt, clat);
+            }
+        
+            CudaDecoder.CloseDecodeHandle(utt);
+
+            processed.pop();
+          }
+
+          nvtxRangePop();
+          if (num_todo!=-1 && num_done>=num_todo) break;
+        } //end utterance loop
+
+      nvtxRangePushA("Lattice Write");
+      while (processed.size()>0) {
+        std::string &utt = processed.front();
+        Lattice lat;
+        CompactLattice clat;
+
+        CudaDecoder.GetBestPath(utt,&lat);
+        ConvertLattice(lat, &clat);
+
+        GetDiagnosticsAndPrintOutput(utt, word_syms, clat, &num_frames, &tot_like);
+
+        if (write_lattice && iter==0 ) {
+          clat_writer.Write(utt, clat);
+        }
+
+        CudaDecoder.CloseDecodeHandle(utt);
+
+        processed.pop();
+
+      } //end for
+      nvtxRangePop();
+      auto finish = std::chrono::high_resolution_clock::now();
+      std::chrono::duration<double> total_time = finish-start;
+      KALDI_LOG << "Iteration: " << iter << " Aggregate Total Time: " << total_time.count()
+        << " Total Audio: " << total_audio 
+        << " RealTimeX: " << total_audio/total_time.count() << std::endl;
+    } //End iterations loop
+    KALDI_LOG << "Decoded " << num_done << " utterances, "
+      << num_err << " with errors.";
+    KALDI_LOG << "Overall likelihood per frame was " << (tot_like / num_frames)
+      << " per frame over " << num_frames << " frames.";
+
+    delete word_syms; // will delete if non-NULL.
+
+    clat_writer.Close();
+
+    CudaDecoder.Finalize();  
+    cudaDeviceSynchronize();
+
+    auto finish = std::chrono::high_resolution_clock::now();
+    nvtxRangePop();
+
+    std::chrono::duration<double> total_time = finish-start;
+
+    KALDI_LOG << "Overall: " << " Aggregate Total Time: " << total_time.count()
+      << " Total Audio: " << total_audio 
+      << " RealTimeX: " << total_audio/total_time.count() << std::endl;
+
+    return 0;
+
+    //return (num_done != 0 ? 0 : 1);
+  } catch (const std::exception& e) {
+    std::cerr << e.what();
+    return -1;
+  }
+} // main()
+
