78e54874172ab9a26eac7a7afe59572c90f82378
jluitjens <jluitjens@nvidia.com>
2019-03-05 12:36:14 -0800

    Added authors

--- src/cudadecoder/batched-threaded-cuda-decoder.h
+++ src/cudadecoder/batched-threaded-cuda-decoder.h
@@ -1,6 +1,7 @@
 // cudadecoder/batched-threaded-cuda-decoder.h
 /*
  * Copyright (c) 2017, NVIDIA CORPORATION.  All rights reserved.
+ * Authors:  Hugo Braun, Justin Luitjens, Ryan Leary
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
--- src/cudadecoder/decodable-cumatrix.h
+++ src/cudadecoder/decodable-cumatrix.h
@@ -1,6 +1,7 @@
 // cudadecoder/decodable-cumatrix.h
 /*
  * Copyright (c) 2017, NVIDIA CORPORATION.  All rights reserved.
+ * Authors:  Hugo Braun, Justin Luitjens, Ryan Leary
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
