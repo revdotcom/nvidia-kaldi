8b7f1ccb8a209eb841d352d30457b840f68b8749
jluitjens <jluitjens@nvidia.com>
2019-03-09 09:01:24 -0800

    Remove exec

--- qa/L0_librispeech/test.sh
+++ qa/L0_librispeech/test.sh
@@ -3,7 +3,7 @@
 cd /workspace/nvidia-examples/librispeech
 
 SKIP_DOWNLOAD=1 SKIP_FLAC2WAV=1 ./prepare_data.sh /data/speech
-exec ./run_benchmark.sh
-exec ./run_multigpu_benchmark.sh
+./run_benchmark.sh
+./run_multigpu_benchmark.sh
 
 #TODO validate results
