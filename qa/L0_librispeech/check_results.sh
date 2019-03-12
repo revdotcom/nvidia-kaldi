dab9821513feca72e017c180315d5729d7c8eac0
jluitjens <jluitjens@nvidia.com>
2019-03-12 11:00:28 -0700

    Regression tests

--- qa/L0_librispeech/check_results.sh
+++ qa/L0_librispeech/check_results.sh
@@ -27,26 +27,30 @@ do
   echo "testname=$testname"
   for (( d = 0 ; d < $NUM_GPUS ; d++ )); do
     WER=`cat /tmp/ls-results.$d/log.batched-wav-nnet3-cuda.$dataset.out | grep "%WER" | cut -d " " -f 2`
-    PERF=`cat /tmp/ls-results.$d/log.batched-wav-nnet3-cuda.$dataset.out | grep Overall | grep Aggregate | cut -d ":" -f 8 | cut -d " " -f 2
+    PERF=`cat /tmp/ls-results.$d/log.batched-wav-nnet3-cuda.$dataset.out | grep Overall | grep Aggregate | cut -d ":" -f 8 | cut -d " " -f 2`
 
     EWER=${EXPECTED_WER[$dataset]}
     EPERF=${EXPECTED_PERF[$testname]}
 
     echo "    dataset=$dataset, GPU=$d: " 
     echo "         WER=$WER, Expected=$EWER"
+    echo "         PERF=$PERF, Expected=$EPERF"
     PASS=`echo "$WER <= $EWER" | bc`
     if [ $PASS -ne "1" ]; then
-      echo "              Error:  WER rate )$WER) greater than  $EWER}"
+      echo "              Error:  WER rate ($WER) greater than  $EWER"
       FAIL=1
     fi
-    echo "         PERF=$PERF, Expected=$EPERF}"
-    PASS=`echo "${PERF} >= $EPERF " | bc`
+    echo "         PERF=$PERF, Expected=$EPERF"
+    PASS=`echo "$PERF >= $EPERF" | bc`
     if [ $PASS -ne "1" ]; then
-      echo "              Error:  PERF ($PERF) less than than  $EPERF}"
+      echo "              Error:  PERF ($PERF) less than than  $EPERF"
       FAIL=1
     fi
   done
 done
 
+if [ $FAIL -eq "0" ]; then
+  echo "All WER and PERF tests passed"
+fi
 exit $FAIL
 
