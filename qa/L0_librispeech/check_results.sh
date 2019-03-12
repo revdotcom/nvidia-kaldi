5547e4a5ec50af760eab9f4bb63cc689d489b6fb
jluitjens <jluitjens@nvidia.com>
2019-03-12 08:58:25 -0700

    Fixes to WER checking

--- qa/L0_librispeech/check_results.sh
+++ qa/L0_librispeech/check_results.sh
@@ -3,24 +3,24 @@
 source gold.inc
 
 num_gpus=`nvidia-smi -L | wc -l`
-data_sets="test_clean test_other"
+datasets="test_clean test_other"
 
 fail=0
 
+echo "CHECKING RESULTS"
 for dataset in $datasets;
 do
   for (( d = 0 ; d < $num_gpus ; d++ )); do
-    WER=`cat /tmp/ls-results.$d | grep "%WER" | cut -d " " -f 2`
-  	pass=`echo "${WER} > ${EXPECTED_WER[$test_set]}" | bc`
-  	
-		echo "dataset=$dataset, WER=$WER, Expected=${EXPECTED_WER[$dataset]}"
+    WER=`cat /tmp/ls-results.$d/log.batched-wav-nnet3-cuda.$dataset.out | grep "%WER" | cut -d " " -f 2`
+    pass=`echo "${WER} > ${EXPECTED_WER[$dataset]}" | bc`
+
+    echo "    dataset=$dataset, GPU=$d, WER=$WER, Expected=${EXPECTED_WER[$dataset]}"
     if [ $pass -ne "0" ]; then
-      echo "  Error:  WER rate $WER great than  ${EXPECTED_WER[$test_set]}"
+      echo "      Error:  WER rate $WER greater than  ${EXPECTED_WER[$dataset]}"
       fail=1
-    else
-      echo "WER rate passed"
     fi
   done
 done
 
 exit $fail
+
