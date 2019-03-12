df3e17dae4ee7e140331fed06f22f30069ae8ac9
jluitjens <jluitjens@nvidia.com>
2019-03-12 08:00:55 -0700

    Fix path for check_results.sh script

--- qa/L0_librispeech/test.sh
+++ qa/L0_librispeech/test.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+pushd .
 cd /workspace/nvidia-examples/librispeech
 
 SKIP_DOWNLOAD=1 SKIP_FLAC2WAV=1 ./prepare_data.sh /data/speech
-bash -ex ./run_benchmark.sh
-bash -ex ./run_multigpu_benchmark.sh
-bash -ex ./check_results.sh
+bash -e ./run_benchmark.sh
+bash -e ./run_multigpu_benchmark.sh
+
+popd
+bash -e ./check_results.sh
