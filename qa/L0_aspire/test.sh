9a6e7e22b2f440ac0df707abef02a853e2ae109e
jluitjens <jluitjens@nvidia.com>
2019-03-13 18:15:40 -0700

    fixed data path for qa tests

--- qa/L0_aspire/test.sh
+++ qa/L0_aspire/test.sh
@@ -5,7 +5,7 @@ ln -s /data/speech/models /workspace/models
 pushd .
 cd /workspace/nvidia-examples/aspire
 
-SKIP_DATA_DOWNLOAD=1 SKIP_FLAC2WAV=1 ./prepare_data.sh /data/speech
+SKIP_DATA_DOWNLOAD=1 SKIP_FLAC2WAV=1 ./prepare_data.sh /data/speech/data
 bash -e ./run_benchmark.sh
 bash -e ./run_multigpu_benchmark.sh
 
--- qa/L0_librispeech/test.sh
+++ qa/L0_librispeech/test.sh
@@ -3,7 +3,7 @@
 pushd .
 cd /workspace/nvidia-examples/librispeech
 
-SKIP_DOWNLOAD=1 SKIP_FLAC2WAV=1 ./prepare_data.sh /data/speech
+SKIP_DOWNLOAD=1 SKIP_FLAC2WAV=1 ./prepare_data.sh /data/speech/data
 bash -e ./run_benchmark.sh
 bash -e ./run_multigpu_benchmark.sh
 
