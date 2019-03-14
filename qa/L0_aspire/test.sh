77a9e8471c410a16183a5095ef067a2790b8b969
jluitjens <jluitjens@nvidia.com>
2019-03-14 08:16:35 -0700

    Skip model download and data download on QA tests

--- qa/L0_aspire/test.sh
+++ qa/L0_aspire/test.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 ln -s /data/speech/models /workspace/models
+ln -s /data/speech/data /workspace/data
 
 pushd .
 cd /workspace/nvidia-examples/aspire
 
-SKIP_DATA_DOWNLOAD=1 SKIP_FLAC2WAV=1 ./prepare_data.sh /data/speech/data
+SKIP_DATA_DOWNLOAD=1 SKIP_FLAC2WAV=1 SKIP_MODEL_DOWNLOAD=1 ./prepare_data.sh
 bash -e ./run_benchmark.sh
 bash -e ./run_multigpu_benchmark.sh
 
--- qa/L0_librispeech/test.sh
+++ qa/L0_librispeech/test.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+ln -s /data/speech/models /workspace/models
+ln -s /data/speech/data /workspace/data
 
 pushd .
 cd /workspace/nvidia-examples/librispeech
 
-SKIP_DOWNLOAD=1 SKIP_FLAC2WAV=1 ./prepare_data.sh /data/speech/data
+SKIP_DATA_DOWNLOAD=1 SKIP_FLAC2WAV=1 SKIP_MODEL_DOWNLOAD=1 ./prepare_data.sh
 bash -e ./run_benchmark.sh
 bash -e ./run_multigpu_benchmark.sh
 
